08780c03ab8f83ed5bd886a9386b272821ccd4a8
VirajRG <viraj.gawde123@gmail.com>
2020-06-27 23:04:16 +0530

    Ui Modifications done

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -14,23 +14,23 @@ import { GithubOutlined } from '@ant-design/icons';
 import './home.scss';
 const { Header, Content, Footer } = Layout;
 
-const MenuMapper = [
-    { key: '0', value: 'Home', icon: <HomeOutlined />, component: <Landing /> },
-    // { key: '1', value: 'Authority', icon: <UserOutlined />, component: <Authority /> },
-    { key: '2', value: 'Campaign', icon: <SoundOutlined /> },
+const AppMenuMapper = [
+    { key: '0', value: 'Home', icon: <HomeOutlined /> },
+    { key: '2', value: 'Donate', icon: <SoundOutlined /> },
     { key: '3', value: 'Explore', icon: <ForkOutlined /> },
     { key: '4', value: 'Track', icon: <FileSearchOutlined /> },
-    { key: '1', value: 'Authority', icon: <UserOutlined />, component: <Authority /> },
-    // { key: '5', value: 'Register', icon: <FileSearchOutlined />, component: <Register /> },
-    // { key: '4', value: 'Track your Donation', icon: <FileSearchOutlined /> },
-    { key: '5', value: 'Root Authority' },
-    { key: '6', value: 'Middle Authority' },
+    { key: '1', value: 'Authority', icon: <UserOutlined /> }
+]
+
+const LandingMenuMapper = [
+    { key: '7', value: 'Docs', link: 'https://github.com' },
+    { key: '8', value: 'View on Github', link: 'https://github.com' },
 ]
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { drawerVisible: false, selectedMenuItem: '0', selectedPage: <Landing /> };
+        this.state = { midAuthName: 'MidAuthName', campaignName: '', isNoAuth: false, isRootAuth: false, isMidAuth: true, drawerVisible: false, selectedMenuItem: '0', selectedPage: <Landing /> };
     }
     // componentDidUpdate(prevProps) {
     //     if (prevProps.changedProp !== this.props.changedProp) {
@@ -46,19 +46,26 @@ class Home extends React.Component {
         this.handleToggleDrawer(false);
         this.setState({ selectedMenuItem: key });
     }
-
+    liveCampClicked = (name, owner) => {
+        this.setState({ selectedMenuItem: '2', campaignName: name, drawerVisible: false });
+    }
     render() {
         return (
             <Layout className="home">
                 <Header className="header">
                     <MenuOutlined className="menu-unfold" type="default" onClick={() => this.handleToggleDrawer(true)} />
-                    <div>Paaradarshak</div>
+                    <div>{this.state.campaignName != '' ? this.state.campaignName : 'Paaradarshak'}</div>
                     <Menu className="desktop-menu" theme="dark" mode="horizontal" selectedKeys={[this.state.selectedMenuItem]}>
-                        {MenuMapper.map((obj) => {
-                            if (parseInt(obj.key) < 5)
+                        {this.state.selectedMenuItem !== '0' ? AppMenuMapper.map((obj) => {
+                            if (parseInt(obj.key) < 5 && parseInt(obj.key) != 1)
                                 return <Menu.Item onClick={() => this.handleChangeMenu(obj.key)} key={obj.key}>{obj.value}</Menu.Item>
-                        })}
-                        <Menu.Item><a href="https://github.com" target="_blank"><GithubOutlined style={{ fontSize: '24px' }} /></a></Menu.Item>
+                            else if (parseInt(obj.key) == 1 && !this.state.isNoAuth)
+                                return <Menu.Item onClick={() => this.handleChangeMenu(obj.key)} key={obj.key}>{obj.value}</Menu.Item>
+
+                        }) :
+                            LandingMenuMapper.map((obj) => {
+                                return <Menu.Item key={obj.key}><a target='_blank' href={obj.link}>{obj.value}</a></Menu.Item>
+                            })}
                     </Menu>
                 </Header>
                 <Drawer title="Welcome, User"
@@ -71,27 +78,28 @@ class Home extends React.Component {
                         selectedKeys={[this.state.selectedMenuItem]}
                         mode="inline"
                         theme="light">
-                        {MenuMapper.map((obj) => {
+                        {this.state.selectedMenuItem !== '0' ? AppMenuMapper.map((obj) => {
                             if (parseInt(obj.key) < 5)
                                 return <Menu.Item onClick={() => this.handleChangeMenu(obj.key)} key={obj.key} icon={obj.icon}>
                                     {obj.value}
                                 </Menu.Item>
+                            else if (parseInt(obj.key) == 1 && !this.state.isNoAuth)
+                                return <Menu.Item onClick={() => this.handleChangeMenu(obj.key)} key={obj.key}>{obj.value}</Menu.Item>
+
+                        }) : LandingMenuMapper.map((obj) => {
+                            return <Menu.Item key={obj.key}><a target='_blank' href={obj.link}>{obj.value}</a></Menu.Item>
                         })}
-                        <Menu.Item key={10} icon={<GithubOutlined />}>
-                            <a href="https://github.com" target="_blank">
-                                Github
-                            </a>
-                        </Menu.Item>
                     </Menu>
                 </Drawer>
-                {this.state.selectedMenuItem == '0' ? <Landing /> :
-                    this.state.selectedMenuItem == '1' ? <Authority handleChangeMenu={this.handleChangeMenu} web3={this.props.web3} /> :
-                        this.state.selectedMenuItem == '3' ? <Tree web3={this.props.web3} /> :
-                            this.state.selectedMenuItem == '2' ? <Campaign web3={this.props.web3} /> :
-                                this.state.selectedMenuItem == '4' ? <Track web3={this.props.web3} /> :
-                                    this.state.selectedMenuItem == '5' ? <RootAuth web3={this.props.web3} labels={this.state.labels} /> :
-                                        this.state.selectedMenuItem == '6' ? <MidAuth web3={this.props.web3} labels={this.state.labels} /> :
-                                            <Register />}
+                {this.state.selectedMenuItem == '0' ? <Landing liveCampClicked={this.liveCampClicked} /> :
+                    this.state.selectedMenuItem == '1' && this.state.isRootAuth ? <RootAuth handleChangeMenu={this.handleChangeMenu} web3={this.props.web3} /> :
+                        this.state.selectedMenuItem == '1' && this.state.isMidAuth ? <MidAuth midAuthName={this.state.midAuthName} web3={this.props.web3} /> :
+                            this.state.selectedMenuItem == '3' ? <Tree web3={this.props.web3} /> :
+                                this.state.selectedMenuItem == '2' ? <Campaign web3={this.props.web3} /> :
+                                    this.state.selectedMenuItem == '4' ? <Track web3={this.props.web3} /> :
+                                        this.state.selectedMenuItem == '5' ? <RootAuth web3={this.props.web3} labels={this.state.labels} /> :
+                                            this.state.selectedMenuItem == '6' ? <MidAuth web3={this.props.web3} labels={this.state.labels} /> :
+                                                <Register />}
             </Layout>
         );
     }
